ece9f5c4efec18773bd07673f282c15b664091f7
PANCHO7532 <pancho7532@protonmail.com>
2021-05-10 11:07:33 -0300

    P7RND_1977700

--- upload.bat
+++ upload.bat
@@ -4,7 +4,7 @@ REM It may not be required or relevant for other projects where this file is spo
 REM You may ask, "why not ignore this file from uploading to GitHub or any git", well... if someone ever wonder why
 REM this project has strange commit names, then, this is the reason.
 TITLE "Upload to git"
-SET /A randomNumber = %RANDOM% * 100 / 32768 + 1
+SET /A randomNumber = %RANDOM% * 100
 ECHO [INFO] Pushing to remote repo, accept? REMEMBER TO REMOVE EVERY TOKEN AND SENSITIVE INFO FFS
 SET /P ANSW="[PROMPT] Write 'yes' but separated if you're ready: "
 IF "%ANSW%" == "y e s" (
